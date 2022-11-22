051352582bdf47239f5fcd3c219ecbb0948a05e8
Black-Male <Gkaranja20@gmail.com>
2022-11-22 12:11:48 +0300

    Java Functions Lesson 2

--- index.js
+++ index.js
@@ -5,17 +5,37 @@ function printValue(value){
 
 // 1.0 FUNCTION DECLARATION
 //TODO: create a basic function that adds two random values
+function RandomValueCreator(){
+    const num1 = 4
+    const num2 = 20
+    console.log(num1+num2)
+} 
 
+RandomValueCreator()
 
 //TODO: create a function that takes in two values and adds them
+function addNumbers(n1,n2){
+    printValue(n1+n2)
+}
 
+addNumbers(5,108)
+addNumbers(76,48990)
 
 //TODO: create a function that takes in two values, adds them and returns the value of the addition
+function addNums(no1,no2){
+    const sum = no1+no2
+    return sum
+}
 
-
+const summation = addNums(72,74)
+printValue(summation)
 // 1.0 FUNCTION EXPRESSION
 //TODO: create a basic function that adds two random values
+const addExp = function (){
+    printValue(310 + 50)
+}
 
+addExp()
 
 //TODO: create a function that takes in two values and adds them
 
@@ -25,8 +45,14 @@ function printValue(value){
 
 // 1.0 ARROW FUNCTION
 //TODO: create a basic function that adds two random values
+const addAnnon = ()=> {
+    printValue(1+1)
+}
+ addAnnon()
 
+ const sayingMyGreetings = () => printValue("gay")
 
+ sayingMyGreetings()
 //TODO: create a function that takes in two values and adds them
 
 
